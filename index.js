01411c696923c0906440dcae2d1e23fe381166b6
root <root@GX-ZU2APPDLV113.glblclouddev.ad.pwcinternal.com>
2018-09-24 13:28:22 +0000

    remove node modules

--- index.js
+++ index.js
@@ -6,10 +6,12 @@
 //   console.log('hi');
 
 import express from "express";
+import cors from "cors";
 import api from "./src/route/api";
 import { movieDb } from "./lib/movie_db/class";
 const movie_db = new movieDb("a9d0c86f5b26f5948534ba1f7be597a2");
 const app = express();
+app.use(cors());
 const route = () => api({ movie_db }, app);
 route();
 app.listen(2000, () => console.log("app running on 2000"));
