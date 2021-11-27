560b93649ba038beae346b29cbc45520ef902266
arnabkdutta <duttaarnab71@gmail.com>
2021-11-27 18:36:46 +0530

    modifined index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from "express";
 import cors from "cors";
 // import { GraphQLServer } from "graphql-yoga";
-import api from "./src/route/api";
-import { movieDb } from "./lib/movie_db/class";
+import api from "./src/route/api.js";
+import { movieDb } from "./lib/movie_db/class.js";
 require("dotenv").config();
 
 const movie_db = new movieDb(process.env.API_KEY);
-// const app = express();
-// app.use(cors());
-// const server = new GraphQLServer({
-//     typeDefs: "./src/schema/schema.graphql",
-//     resolvers
-//   });
-// server.start(() => console.log(`Server is running on http://localhost:4000`));
 
 const app = express();
 app.use(cors());
